473d7568ca7afc2a4db53ff45982f1aaf79626ee
Replit user <>
2023-11-22 16:00:17 +0000

    MOD Se agrega ejecucion flexible

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-import subprocess
 import os
+import subprocess
 
+# Se obtiene la ruta donde se esta ejecutando el script
 directorio = subprocess.run(["pwd"], stdout=subprocess.PIPE)
-print(directorio.stdout.decode("utf-8"))
+directorio = directorio.stdout.decode().strip()
 
-os.chdir('/home/runner/scrapermyl/cartas_myl')
+# Se busca el directorio spiders con una profundidad de 3 
+buscar = subprocess.run(
+    ['find', directorio, '-maxdepth','3', '-type','d', '-name','spiders'],
+    stdout=subprocess.PIPE)
+
+# Se cambia de directorio al que se encuentra el spider
+sbuscar = buscar.stdout.decode().strip().split("/")
+os.chdir(os.path.join(directorio, sbuscar[-2]))
+
+# Se ejecuta el spider
 process = subprocess.Popen(['scrapy', 'crawl', 'myl'],
                            stdout=subprocess.PIPE,
                            stderr=subprocess.STDOUT,
@@ -16,5 +26,5 @@ for line in process.stdout:
 return_code = process.wait()
 
 if process.returncode != 0:
-  
+
   print(process.stderr)
