60964f92988aacbdb06415a9d07290e539ad00b7
Replit user <>
2023-11-23 04:59:25 +0000

    MOD se corrige ejecucion linux

--- main.py
+++ main.py
@@ -2,36 +2,36 @@ import os
 import subprocess
 
 if os.name == 'nt':
-  directorio = os.getcwd().replace('\\','/').strip()
-  buscar = subprocess.run(
-    ["powershell", "-Command", f"Get-ChildItem", "-Path", f"'{directorio}'", "-Recurse","-Directory","-Filter","'spiders'"],
-    stdout=subprocess.PIPE,
-    text=True
-  )
-  buscar.stdout.replace('\\','/').strip()
+  directorio = os.getcwd().replace('\\', '/').strip()
+  buscar = subprocess.run([
+      "powershell", "-Command", f"Get-ChildItem", "-Path", f"'{directorio}'",
+      "-Recurse", "-Directory", "-Filter", "'spiders'"
+  ],
+                          stdout=subprocess.PIPE,
+                          text=True)
+  buscar.stdout.replace('\\', '/').strip()
   sbuscar = buscar.stdout.strip().split("\\")
   os.chdir(os.path.join(directorio, sbuscar[-2]))
-  
+
 else:
   # Se obtiene la ruta donde se esta ejecutando el script
   directorio = subprocess.run(["pwd"], stdout=subprocess.PIPE)
   directorio = directorio.stdout.decode().strip()
 
-  # Se busca el directorio spiders con una profundidad de 3 
+  # Se busca el directorio spiders con una profundidad de 3
   buscar = subprocess.run(
-      ['find', directorio, '-type','d', '-name','spiders'],
+      ['find', directorio, '-maxdepth', '3', '-type', 'd', '-name', 'spiders'],
       stdout=subprocess.PIPE)
 
   # Se cambia de directorio al que se encuentra el spider
-  sbuscar = buscar.stdout.strip().split("/")
+  sbuscar = buscar.stdout.decode().strip().split("/")
   print(sbuscar)
   os.chdir(os.path.join(directorio, sbuscar[-2]))
 
 # Se cambia de directorio al que se encuentra el spider
 
-
 # Se ejecuta el spider
-process = subprocess.Popen(['scrapy', 'crawl', 'myl','-L','INFO'],
+process = subprocess.Popen(['scrapy', 'crawl', 'myl', '-L', 'INFO'],
                            stdout=subprocess.PIPE,
                            stderr=subprocess.STDOUT,
                            text=True)
